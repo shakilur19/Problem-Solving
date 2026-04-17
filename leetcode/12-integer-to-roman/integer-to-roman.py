10dddb6403d798f317a2f55c5441f19106047e32
Md Shakilur Rahman <shakilur19@gmail.com>
2026-04-17 01:48:26 -0500

    Time: 3 ms (77.93%) | Memory: 19.4 MB (35.03%) - LeetSync

--- leetcode/12-integer-to-roman/integer-to-roman.py
+++ leetcode/12-integer-to-roman/integer-to-roman.py
@@ -1,30 +1,13 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        mp = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-        n = num
-        arr = []
-        pos = 0
-
-        while n > 0:
-            rem = n % 10
-            pos += 1
-            arr.append((rem, pos))
-            n //= 10
-
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
         res = ""
-
-        for i in range(len(arr) - 1, -1, -1):
-            digit = arr[i][0]
-            base = 10 ** (arr[i][1] - 1)
-            if digit <= 3:
-                res += mp[base] * digit
-            elif digit == 4:
-                res += mp[base] + mp[5 * base]
-            elif digit == 5:
-                res += mp[5 * base]
-            elif digit <= 8:
-                res += mp[5 * base] + mp[base] * (digit - 5)
-            elif digit == 9:
-                res += mp[base] + mp[10 * base]
-
-        return res
+        
+        for i in range(len(values)):
+            while num >= values[i]:
+                res += symbols[i]
+                num -= values[i]
+        
+        return res 
